299efdc4f733e25a82f0677f0be1fd03ccc7cb85
Masterchef365 <duncan.freeman1@gmail.com>
2025-07-25 01:12:18 -0700

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use egui::DragValue;
+use egui::{pos2, DragValue, Vec2};
+use egui_euc::SoftwareGui;
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
@@ -19,7 +20,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
     )
 }
 
@@ -49,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(App::new(cc)))),
             )
             .await;
 
@@ -70,59 +71,88 @@ fn main() {
     });
 }
 
-pub struct TemplateApp {
-    pub save_data: SaveData,
+struct App {
+    tex: egui::TextureId,
+    sub: SoftwareGui,
+    demo: egui_demo_lib::DemoWindows,
+    dims: [usize; 2],
 }
 
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct SaveData {
-    example_value: f32,
-}
+impl App {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        let options = egui::TextureOptions::NEAREST;
+        let dims = [640, 480];
+        let image = egui::ImageData::from(egui::ColorImage::filled(
+            dims,
+            egui::Color32::RED,
+        ));
 
-impl Default for SaveData {
-    fn default() -> Self {
+        let tex = cc
+            .egui_ctx
+            .tex_manager()
+            .write()
+            .alloc("sub-gui".into(), image, options);
+
         Self {
-            // Example stuff:
-            example_value: 2.7,
+            demo: Default::default(),
+            tex,
+            dims,
+            sub: SoftwareGui::new(),
         }
     }
 }
 
-impl TemplateApp {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let save_data = cc
-            .storage
-            .and_then(|storage| eframe::get_value(storage, eframe::APP_KEY))
-            .unwrap_or_default();
-
-        Self { save_data }
-    }
-}
-
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, &self.save_data);
-    }
-
-    /// Called each time the UI needs repainting, which may be many times per second.
+impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                    }
-                });
-
-                egui::widgets::global_theme_preference_buttons(ui);
-            });
-        });
-
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("hi world or whatever");
-            ui.add(DragValue::new(&mut self.save_data.example_value).prefix("Example number: "));
+            ui.strong("Resolution:");
+            ui.horizontal(|ui| {
+                ui.add(DragValue::new(&mut self.dims[0]));
+                ui.label("x");
+                ui.add(DragValue::new(&mut self.dims[1]));
+            });
+            let [width, height] = self.dims;
+
+            let (rect, _) = ui.allocate_exact_size(
+                Vec2::new(width as _, height as _) / ui.pixels_per_point(),
+                egui::Sense::click_and_drag(),
+            );
+
+            ui.painter().image(
+                self.tex,
+                rect,
+                egui::Rect::from_min_max(pos2(0.0, 0.0), pos2(1.0, 1.0)),
+                egui::Color32::WHITE,
+            );
+
+            ui.painter().rect_stroke(rect, 0.0, egui::Stroke::new(1.0, egui::Color32::WHITE), egui::StrokeKind::Outside);
+
+            let mut raw_input = ctx.input(|r| r.raw.clone());
+
+            raw_input.screen_rect = Some(egui::Rect::from_min_size(egui::Pos2::ZERO, Vec2::new(width as f32, height as f32) / ui.pixels_per_point()));
+
+            for event in &mut raw_input.events {
+                match event {
+                    egui::Event::PointerMoved(pos) => {
+                        *pos -= rect.min.to_vec2();
+                    },
+                    egui::Event::PointerButton { pos, .. } => {
+                        *pos -= rect.min.to_vec2();
+                    }
+                    _ => (),
+                }
+            }
+
+            //self.sub.egui_ctx.set_pixels_per_point(ui.pixels_per_point());
+            let new_image = self.sub.update(raw_input, self.dims, |ctx| {
+                self.demo.ui(ctx);
+            });
+
+            ui.ctx().tex_manager().write().set(
+                self.tex,
+                egui::epaint::ImageDelta::full(new_image, egui::TextureOptions::NEAREST),
+            );
         });
     }
 }
+
